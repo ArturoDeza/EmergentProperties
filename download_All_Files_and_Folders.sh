bbbd8df2af1cc9e8d7007bc92f2749e2f41af343
Arturo <arturodeza@gmail.com>
2021-06-22 16:51:56 -0400

    Download All Files and Folders

---
+++ download_All_Files_and_Folders.sh
@@ -0,0 +1,51 @@
+# Download entire folder to reproduce results from "Emergent Properties of Foveated Perceptual Systems"
+
+# Download Training:
+wget https://www.dropbox.com/s/vot6o7o9yfcvwk7/Training.tar.gz?dl=1
+
+# Download Generalization:
+wget https://www.dropbox.com/s/6bs5zmfiraqgi9o/Generalization.tar.gz?dl=1
+
+# Download Spatial Frequency:
+wget https://www.dropbox.com/s/pxgsx380qbfjeqf/Spatial_Frequency.tar.gz?dl=1
+
+# Download Robustness to Occlusion:
+wget https://www.dropbox.com/s/y5tlrdgm6vmojbq/Robustness_to_Occlusion.tar.gz?dl=1
+
+# Download Window Cue Conflict:
+wget https://www.dropbox.com/s/sno9wg8qxsvn82q/Window_Cue_Conflict.tar.gz?dl=1
+
+# Download Square Cue Conflict:
+wget https://www.dropbox.com/s/4ae253nvb19dgy6/Square_Cue_Conflict.tar.gz?dl=1
+
+# Download Perceptual Optimization
+wget https://www.dropbox.com/s/lelknxtqa6kyod9/Perceptual_Optimization.tar.gz?dl=1
+
+# Download Dataset Files
+wget https://www.dropbox.com/s/okgmc3e6em39w7x/Dataset_Files.tar.gz
+
+# Download Network Checkpoints:
+mkdir All_Networks
+cd All_Networks
+wget https://www.dropbox.com/s/cl2jvtywqok5uhw/Reference-Net.tar.gz?dl=1
+wget https://www.dropbox.com/s/zh3rgib5w724jt1/Foveation-Texture-Net.tar.gz?dl=1
+wget https://www.dropbox.com/s/zh3rgib5w724jt1/Foveation-Texture-Net.tar.gz?dl=1
+wget https://www.dropbox.com/s/btr88ezn8rxin8y/Foveation-Blur-Net.tar.gz?dl=1
+cd ..
+
+# Download Training Images
+mkdir All_Training_Images
+cd All_Training_Images
+wget https://www.dropbox.com/s/hnd53grpw05jb8h/Training_Reference.tar.gz?dl=1
+wget https://www.dropbox.com/s/lgl1k8bep14uuc0/Training_Foveation_Texture.tar.gz?dl=1
+wget https://www.dropbox.com/s/qqrxsli6raum5vd/Training_Uniform_Blur.tar.gz?dl=1
+wget https://www.dropbox.com/s/8wijwjec8d51x9o/Training_Foveation_Blur.tar.gz?dl=1
+cd ..
+
+# Download Validation Images
+wget https://www.dropbox.com/s/76987s3w4i6myku/All_Validation_Images.tar.gz?dl=1
+
+# Download Testing Images
+wget https://www.dropbox.com/s/yza9cppfjpbn3vj/All_Testing_Images.tar.gz?dl=1
+
+
